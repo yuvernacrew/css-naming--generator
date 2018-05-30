1db2f1ea69ffac667c045b07efa92fdde5f13d20
加藤　優奈 <yuvernacrew@gmail.com>
2018-05-30 15:08:53 +0900

    translate message on cliant

--- app/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <p>こんにちは</p>
-    <p>ちーずだよ</p>
-  </div>
-</template>
-<script>
-import Vue from 'vue'
-</script>
--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import App from './App'
 import 'babel-polyfill';
+import axios from 'axios';
 
 new Vue({
   el: '#app',
   components: { App },
+  watch: {
+    // val = 新しい入力値
+    // oldVal = 古い入力値
+    text: function (val) {
+      axios
+      .get('/api/'+ val)
+      .then(response => (this.text_translate = response))
+    }
+  },
   data: {
-    hello: "こんにちは世界"
+    text: "",
+    text_translate : "",
   }
 })
